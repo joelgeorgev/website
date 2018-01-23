9083723a7d3053c54b508d0195ff14f6bc3a462f
Joel George V <joel.gv3@gmail.com>
2018-01-23 23:12:42 +0530

    Update to test CI/CD

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'tachyons/css/tachyons.min.css';
 export class App extends React.PureComponent {
   render() {
     return (
-      <div className='b'>
+      <div className='b tc'>
         Joel George V
       </div>
     );
