0603806a9550e66412987f0e9aa99499cc660f2e
Joel George V <joel.gv3@gmail.com>
2018-01-23 22:27:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'tachyons/css/tachyons.min.css';
+
+export class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        Joel George V
+      </div>
+    );
+  }
+}
