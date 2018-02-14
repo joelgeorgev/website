f1bad82763478f628a2b3a657f117523cee1de59
Joel George V <joel.gv3@gmail.com>
2018-02-14 23:20:18 +0530

    Update template

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import 'tachyons/css/tachyons.min.css';
 export class App extends React.PureComponent {
   render() {
     return (
-      <div className='b tc'>
-        Joel George V
+      <div className='pa4 flex center w-80 mw8 vh-100'>
+        <div className='w-100 tr pa4 br b--light-gray bw2'>
+          <span className='b'>Light Side</span>
+          <p>"The quick brown fox jumps over the lazy dog" is an English-language pangram—a sentence that contains all of the letters of the alphabet. It is commonly used for touch-typing practice, testing typewriters and computer keyboards, displaying examples of fonts, and other applications involving text where the use of all letters in the alphabet is desired. Owing to its brevity and coherence, it has become widely known.</p>
+        </div>
+        <div className='w-100 pa4'>
+          <span className='b'>Dark Side</span>
+          <p>"The quick brown fox jumps over the lazy dog" is an English-language pangram—a sentence that contains all of the letters of the alphabet. It is commonly used for touch-typing practice, testing typewriters and computer keyboards, displaying examples of fonts, and other applications involving text where the use of all letters in the alphabet is desired. Owing to its brevity and coherence, it has become widely known.</p>
+        </div>
       </div>
     );
   }
