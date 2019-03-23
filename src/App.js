2bb2c0957eff2a487a837ce9cb25c661771bf709
Joel George V <joel.gv3@gmail.com>
2019-03-23 11:12:08 +0000

    Use function components

--- src/App.js
+++ src/App.js
@@ -6,25 +6,23 @@ import github from './assets/github.svg'
 import instagram from './assets/instagram.svg'
 import twitter from './assets/twitter.svg'
 
-export class App extends React.PureComponent {
-  render() {
-    return (
-      <div style={{ backgroundImage: `url(${background})` }} className='flex flex-column justify-center w-100 vh-100 bg-center cover pv4'>
-        <div className='flex flex-auto flex-column justify-center items-center white'>
-          <div className='f2'>Joel George V</div>
-        </div>
-        <div className='flex justify-center'>
-          <a className='mh1' href='https://github.com/joelgeorgev'>
-            <img src={github} alt='GitHub' />
-          </a>
-          <a className='mh1' href='https://www.instagram.com/joelgeorgev'>
-            <img src={instagram} alt='Instagram' />
-          </a>
-          <a className='mh1' href='https://twitter.com/joelgeorgev'>
-            <img src={twitter} alt='Twitter' />
-          </a>
-        </div>
+export const App = () => {
+  return (
+    <div style={{ backgroundImage: `url(${background})` }} className='flex flex-column justify-center w-100 vh-100 bg-center cover pv4'>
+      <div className='flex flex-auto flex-column justify-center items-center white'>
+        <div className='f2'>Joel George V</div>
       </div>
-    )
-  }
+      <div className='flex justify-center'>
+        <a className='mh1' href='https://github.com/joelgeorgev'>
+          <img src={github} alt='GitHub' />
+        </a>
+        <a className='mh1' href='https://www.instagram.com/joelgeorgev'>
+          <img src={instagram} alt='Instagram' />
+        </a>
+        <a className='mh1' href='https://twitter.com/joelgeorgev'>
+          <img src={twitter} alt='Twitter' />
+        </a>
+      </div>
+    </div>
+  )
 }
