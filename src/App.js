cc9e3767f889a0660558f430240f506063e5039b
Joel George V <joel.gv3@gmail.com>
2019-03-25 21:10:06 +0000

    Use webp as background image with fallback color

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import 'tachyons/css/tachyons.min.css'
 
-import background from './assets/gabriele-garanzelli-529492-unsplash.jpg'
 import github from './assets/github.svg'
 import instagram from './assets/instagram.svg'
 import twitter from './assets/twitter.svg'
 
 export const App = () => {
   return (
-    <div style={{ backgroundImage: `url(${background})` }} className='flex flex-column justify-center w-100 vh-100 bg-center cover pv4'>
+    <div className='bg-image flex flex-column justify-center w-100 vh-100 bg-center cover pv4'>
       <div className='flex flex-auto flex-column justify-center items-center white'>
         <div className='f2'>Joel George V</div>
       </div>
