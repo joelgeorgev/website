8e0d09922af06513ca7a5cf9504b7eab3f03b308
Joel George V <joel.gv3@gmail.com>
2019-03-25 23:30:19 +0000

    Use state, effect hooks along with css animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import 'tachyons/css/tachyons.min.css'
 
 import github from './assets/github.svg'
@@ -6,12 +6,26 @@ import instagram from './assets/instagram.svg'
 import twitter from './assets/twitter.svg'
 
 export const App = () => {
+  const [name, setName] = useState(false)
+  const [social, setSocial] = useState(false)
+
+  useEffect(() => {
+    const onLoad = () => {
+      setName(true)
+      setSocial(true)
+    }
+
+    window.addEventListener('load', onLoad)
+    return () => window.removeEventListener('load', onLoad)
+  }, [])
+
   return (
     <div className='bg-image flex flex-column justify-center w-100 vh-100 bg-center cover pv4'>
-      <div className='flex flex-auto flex-column justify-center items-center white'>
+      <div className={`name o-0 ${name ? 'name-active' : ''} 
+      flex flex-auto flex-column justify-center items-center white`}>
         <div className='f2'>Joel George V</div>
       </div>
-      <div className='flex justify-center'>
+      <div className={`social o-0 ${social ? 'social-active' : ''} flex justify-center`}>
         <a className='mh1' href='https://github.com/joelgeorgev'>
           <img src={github} alt='GitHub' />
         </a>
