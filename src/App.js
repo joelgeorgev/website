c77d9b47db202e3ade2b220738fc4cf1019df307
Joel George V <joel.gv3@gmail.com>
2019-03-26 20:30:08 +0000

    Rename variables

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import instagram from './assets/instagram.svg'
 import twitter from './assets/twitter.svg'
 
 export const App = () => {
-  const [name, setName] = useState(false)
-  const [social, setSocial] = useState(false)
+  const [showName, toggleShowName] = useState(false)
+  const [showSocial, toggleShowSocial] = useState(false)
 
   useEffect(() => {
     const onLoad = () => {
-      setName(true)
-      setSocial(true)
+      toggleShowName(true)
+      toggleShowSocial(true)
     }
 
     window.addEventListener('load', onLoad)
@@ -21,11 +21,11 @@ export const App = () => {
 
   return (
     <div className='bg-image flex flex-column justify-center w-100 vh-100 bg-center cover pv4'>
-      <div className={`name o-0 ${name ? 'name-active' : ''} 
+      <div className={`name o-0 ${showName ? 'name-active' : ''} 
       flex flex-auto flex-column justify-center items-center white`}>
         <div className='f2'>Joel George V</div>
       </div>
-      <div className={`social o-0 ${social ? 'social-active' : ''} flex justify-center`}>
+      <div className={`social o-0 ${showSocial ? 'social-active' : ''} flex justify-center`}>
         <a className='mh1' href='https://github.com/joelgeorgev'>
           <img src={github} alt='GitHub' />
         </a>
