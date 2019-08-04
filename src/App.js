d8a207fa937e431dec7e1aa07f17e95898718ee2
Joel George V <joel.gv3@gmail.com>
2019-08-04 01:40:45 +0100

    Use concise body for arrow functions

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import 'tachyons/css/tachyons.min.css'
 
 import { Name, Social } from './components'
 
-export const App = () => {
-  return (
-    <main role='main' className='vh-100 overflow-y-auto scroll-snap-container'>
-      <Name />
-      <Social />
-    </main>
-  )
-}
+export const App = () => (
+  <main role='main' className='vh-100 overflow-y-auto scroll-snap-container'>
+    <Name />
+    <Social />
+  </main>
+)
