738c0313588783e57564cd34565fa5dcddc70921
Joel George V <joel.gv3@gmail.com>
2023-12-30 11:02:15 +0000

    Add about page

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('CNAME')
   eleventyConfig.addPassthroughCopy('fonts')
   eleventyConfig.addPassthroughCopy('assets')
+
+  return {
+    pathPrefix: '/website/'
+  }
 }
